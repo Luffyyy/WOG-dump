b2c3b3873a89848dca447f918382ebe2f2c6546a
Artem <56561445+hampta@users.noreply.github.com>
2023-10-01 04:13:22 +0300

    fast fix for xor

--- xor.c
+++ xor.c
@@ -7,64 +7,20 @@
 // thx github copilot
 
 int main(int argc, char *argv[]) {
-    FILE *fp, *fp2;
+    FILE *file = fopen(argv[1], "rb");
+    FILE *output = fopen(argv[3], "wb");
     char *key = argv[2];
-    char *output_file = argv[3];
-    char *encrypted_file = argv[1];
-    char *buffer;
-    char *buffer2;
+    int key_len = strlen(key);
+    int c;
     int i = 0;
-    int j = 0;
-    int key_length = strlen(key);
-    int file_length = 0;
-    int file_length2 = 0;
-    int xor = 0;
 
-    fp = fopen(encrypted_file, "r");
-    fp2 = fopen(output_file, "w");
-
-    if (fp == NULL) {
-        printf("Error opening file\n");
-        exit(1);
+    while ((c = fgetc(file)) != EOF) {
+        fputc(c ^ key[i], output);
+        i = (i + 1) % key_len;
     }
 
-    if (fp2 == NULL) {
-        printf("Error opening file\n");
-        exit(1);
-    }
-
-    // get file length
-    fseek(fp, 0, SEEK_END);
-    file_length = ftell(fp);
-    fseek(fp, 0, SEEK_SET);
-
-    // allocate memory for buffer
-    buffer = (char *)malloc(file_length * sizeof(char));
-    buffer2 = (char *)malloc(file_length * sizeof(char));
-
-    // read file into buffer
-    fread(buffer, file_length, 1, fp);
-
-    // XOR decryption
-    for (i = 0; i < file_length; i++) {
-        xor = buffer[i] ^ key[j];
-        buffer2[i] = xor;
-        j++;
-        if (j == key_length) {
-            j = 0;
-        }
-    }
-
-    // write buffer to file
-    fwrite(buffer2, file_length, 1, fp2);
-
-    // free memory
-    free(buffer);
-    free(buffer2);
-
-    // close files
-    fclose(fp);
-    fclose(fp2);
-
+    fclose(file);
+    fclose(output);
     return 0;
 }
+
